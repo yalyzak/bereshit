a98a619f4cf35aa96855e60c6624d9a437a57489
yalyzak <132987109+yalyzak@users.noreply.github.com>
2025-09-20 19:11:34 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,62 +1,44 @@
+import selectors
 import socket
-import select
-import time
-from content_filter import ContentFilter
-from user_manager import UserManager
-from clientHandler import ClientHandler
-class Server:
-    def __init__(self,HOST = '127.0.0.1',PORT = 12345,user_manager=None,filter=None,hendler=None):
-        self.HOST =HOST
-        self.PORT = PORT
-        self.attach(user_manager,filter,hendler)
-    def attach(self,user_manager,filter,hendler):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.HOST, self.PORT))
-        self.server_socket.listen()
-        print(f"Server started on {self.HOST}:{self.PORT}")
 
-        self.user_manager = user_manager if user_manager else UserManager()
-        self.filter = filter if filter else ContentFilter()
-        self.hendler = hendler if hendler else ClientHandler(self.server_socket)
+sel = selectors.DefaultSelector()
 
-        # Stores sockets flagged for delayed kick: {socket: timestamp_sent_message}
-        self.users_to_kick = {}
+def accept(sock):
+    conn, addr = sock.accept()
+    print("Accepted connection from", addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)
 
-
-
-    def hander_msg(self,header,message,ip,hendler,notified_socket):
-        if header == 1:
-            if self.filter.is_message_clean(message):
-                hendler.clients[notified_socket]["username"] = message
-                hendler.clients[notified_socket]["ip"] = ip
-                hendler.broadcast(notified_socket, f"[SYSTEM] {message} username had joined")
-            else:
-                hendler.broadcast(notified_socket, f"[SYSTEM] {message} username is not Valid please pick another username")
-                hendler.clients[notified_socket]["username"] = "john doe"
-
-
-        elif header == 2:
-            hendler.broadcast(notified_socket, f"[SYSTEM] {hendler.clients[notified_socket]['username']} has changed its name to {message}")
-            hendler.clients[notified_socket]["username"] = message
+def read(conn):
+    try:
+        data = conn.recv(1024)
+        if data:
+            print("Received:", data.decode())
+            conn.sendall(data)
         else:
-            username = hendler.clients[notified_socket]["username"]
-            if self.filter.is_message_clean(message):
-                hendler.broadcast(notified_socket, f"{username}: {message}")
-            else:
-                ip = hendler.clients[notified_socket]["ip"]
-                kick, violations = self.user_manager.report_offense(username, ip)
-                if kick:
-                    try:
-                        notified_socket.send("[SYSTEM] You have been removed for inappropriate behavior.".encode())
-                        self.users_to_kick[notified_socket] = time.time()
-                        return notified_socket
-                    except:
-                        hendler.cleanup_client(notified_socket)
-                else:
-                    try:
-                        notified_socket.send(
-                            f"[WARNING] You have {violations} violations, extensive violations will cause ban.".encode())
-                    except:
-                        pass
+            print("Closing connection")
+            sel.unregister(conn)
+            conn.close()
+    except ConnectionResetError:
+        print("Client reset connection")
+        sel.unregister(conn)
+        conn.close()
 
 
+# Create listening socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(("0.0.0.0", 5000))
+sock.listen()
+sock.setblocking(False)
+
+# Register the listening socket for READ events
+sel.register(sock, selectors.EVENT_READ, accept)
+
+print("Server listening on port 5000...")
+
+# Event loop
+while True:
+    events = sel.select(timeout=None)  # Blocks until something is ready
+    for key, mask in events:
+        callback = key.data  # The function we attached (accept/read)
+        callback(key.fileobj)
