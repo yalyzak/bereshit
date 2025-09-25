2a61af74f27d569b4fc8f95e5924aac6d4fae4dd
yalyzak <132987109+yalyzak@users.noreply.github.com>
2025-09-25 14:11:58 +0300

    Delete Trap.py

--- Trap.py
+++
@@ -1,7 +0,0 @@
-from bereshit import Vector3
-class Trap:
-
-    def OnTriggerEnter(self, other):
-        if other.parent.name == "car":
-            print("💀 You fell into the trap!")
-            other.parent.reset_to_default()
