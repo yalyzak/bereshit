2f99410286e3aef41c6180127402e873c13a147e
yalyzak <yaly.zak@gmail.com>
2026-03-10 22:20:08 +0200

    add input system and fixed ui and organised the for to functions

--- render.py
+++ render.py
@@ -3,6 +3,7 @@ import importlib
 import sys
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
+from collections import deque
 from importlib.resources import files
 
 for name in [
@@ -70,6 +71,7 @@ class BereshitRenderer(moderngl_window.WindowConfig):
         texture.repeat_y = True
 
         return texture
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.wnd.exit_key = None
@@ -80,8 +82,9 @@ class BereshitRenderer(moderngl_window.WindowConfig):
             raise Exception("No camera object found")
         self.cam = self.camera_obj.Camera
         self.cam.render = self
-        self.Initialize[0] = True
 
+        self.text_input = 0
+        self.typing = False
 
         self.fov = self.cam.FOV
         self.viewer_distance = self.cam.VIEWER_DISTANCE
@@ -190,53 +193,8 @@ class BereshitRenderer(moderngl_window.WindowConfig):
         self.texture = self.ctx.texture(self.window_size, 4)
         self.texture.filter = (moderngl.LINEAR, moderngl.LINEAR)
         self.default_texture = self.load_texture((str(files("bereshit")) + "\\shaders" + "\\default_texture.jpg"))
-    def GetKeyDown(self):
-        keys = self.wnd.keys
-        string = ""
-        for key in self.keys_down:
 
-            key_name = None
-            for name, value in vars(keys).items():
-                if value == key:
-                    key_name = name
-                    break
-
-            # Fallback to printing the raw key code
-            if key_name is None:
-                key_name = f"Unknown({key})"
-
-            string += key_name
-        self.keys_down = set()
-        return string
-
-    def GetKey(self):
-        keys = self.wnd.keys
-        string = ""
-        for key in self.keys:
-            key_name = None
-            for name, value in vars(keys).items():
-                if value == key:
-                    key_name = name
-                    break
-
-            # Fallback to printing the raw key code
-            if key_name is None:
-                key_name = f"Unknown({key})"
-
-            string += key_name
-        self.keys = list()
-        return string
-
-    def on_key_event(self, key, action, modifiers):
-        keys = self.wnd.keys
-        self.keys.append(key)
-
-        # Key pressed (only trigger once)
-        if action == keys.ACTION_PRESS and key not in self.keys_down:
-            self.keys_down.add(key)
-        # Key released (remove from pressed set)
-        elif action == keys.ACTION_RELEASE and key in self.keys_down:
-            self.keys_down.remove(key)
+        self.Initialize[0] = True # must be at the end
 
     def wire_shading(self, objs):
         for obj in objs:
@@ -295,7 +253,7 @@ class BereshitRenderer(moderngl_window.WindowConfig):
                     'len': len(triangles) // 3,
                 })
 
-    def material_preview(self, objs):
+    def material_preview_shading(self, objs):
         for obj in objs:
             if obj.Mesh is None:
                 continue
@@ -337,6 +295,79 @@ class BereshitRenderer(moderngl_window.WindowConfig):
                 'len': len(vertex_data) // 5,
             })
 
+    def render_mesh(self, item, shading, cam_pos):
+        obj = item['obj']
+        pos = obj.position.to_np()
+        size = obj.size.to_np()
+        rot = obj.quaternion
+
+        model = (
+                Matrix44.from_scale(size * 0.5)
+                @ Matrix44.from_quaternion(PyrrQuat([rot.x, rot.y, rot.z, rot.w]))
+                @ Matrix44.from_translation(pos)
+        )
+        if shading == "wire":
+            self.wire_rendering(item, model, obj)
+        elif shading == "solid":
+            self.solid_rendering(item, model, obj, cam_pos)
+        elif shading == "material preview":
+            self.material_preview_rendering(item, model, obj, cam_pos)
+
+    def wire_rendering(self, item, model, obj):
+        item['vao'].render(mode=moderngl.LINES, vertices=item['len'])
+        self.wire_prog['model'].write(model.astype('f4').tobytes())
+        self.wire_prog['view'].write(self.view.astype('f4').tobytes())
+        self.wire_prog['projection'].write(self.projection.astype('f4').tobytes())
+        # self.prog['lightPos'].value = cam_pos  # your light source coordinates
+
+        color = obj.material.color if hasattr(obj.material, 'color') else (1.0, 1.0, 1.0)
+        self.wire_prog['color'].value = color
+
+    def solid_rendering(self, item, model, obj, cam_pos):
+        # Enable depth test
+        self.ctx.enable(moderngl.DEPTH_TEST)
+
+        # Set uniforms before drawing
+        self.solid_prog['model'].write(model.astype('f4').tobytes())
+        self.solid_prog['view'].write(self.view.astype('f4').tobytes())
+        self.solid_prog['projection'].write(self.projection.astype('f4').tobytes())
+
+        self.solid_prog['light_pos'].value = cam_pos
+
+        self.solid_prog['light_color'].value = (1.0, 1.0, 1.0)
+        color = obj.material.color if hasattr(obj.material, 'color') else (1.0, 1.0, 1.0)
+        self.solid_prog['object_color'].value = color
+        self.solid_prog['view_pos'].value = tuple(cam_pos)
+
+        # Render as filled triangles
+        item['vao'].render(mode=moderngl.TRIANGLES, vertices=item['len'])
+
+    def material_preview_rendering(self, item, model, obj, cam_pos):
+        self.ctx.enable(moderngl.DEPTH_TEST)
+
+        # Set matrices
+        self.material_prog['model'].write(model.astype('f4').tobytes())
+        self.material_prog['view'].write(self.view.astype('f4').tobytes())
+        self.material_prog['projection'].write(self.projection.astype('f4').tobytes())
+
+        # Lighting uniforms
+        # self.material_prog['light_pos'].value = tuple(cam_pos)
+        # self.material_prog['light_color'].value = (1.0, 1.0, 1.0)
+        # self.material_prog['view_pos'].value = tuple(cam_pos)
+
+        # Bind texture
+        texture = obj.Mesh.texture()
+
+        if texture:
+            if not hasattr(obj.Mesh, "gpu_texture"):
+                obj.Mesh.gpu_texture = self.image_to_texture(texture)
+
+            obj.Mesh.gpu_texture.use(location=0)
+        else:
+            self.default_texture.use(location=0)
+        # Draw mesh
+        item['vao'].render(mode=moderngl.TRIANGLES, vertices=item['len'])
+
     def prepare_meshes(self):
         shading = self.cam.shading
         objs = self.root_object.get_all_children()
@@ -345,7 +376,7 @@ class BereshitRenderer(moderngl_window.WindowConfig):
         elif shading == "solid":
             self.solid_shading(objs)
         elif shading == "material preview":
-            self.material_preview(objs)
+            self.material_preview_shading(objs)
 
     def prepare_missing_meshes(self, missing):
         shading = self.cam.shading
@@ -353,6 +384,8 @@ class BereshitRenderer(moderngl_window.WindowConfig):
             self.wire_shading(missing)
         elif shading == "solid":
             self.solid_shading(missing)
+        elif shading == "material preview":
+            self.material_preview_shading(missing)
 
     def cleanup_removed_meshes(self, removed_objs):
         self.meshes = [m for m in self.meshes if m['obj'] not in removed_objs]
@@ -361,93 +394,73 @@ class BereshitRenderer(moderngl_window.WindowConfig):
         self.projection = Matrix44.perspective_projection(self.fov, self.wnd.aspect_ratio, 0.1, 1000.0)
         self.ortho_projection = Matrix44.orthogonal_projection(0, width, 0, height, -1.0, 1.0)
 
-    # def render_text(self):
-    #     if not self.text_elements:
-    #         return
-    #     self.texture.use()
-    #     self.text_vbo.render(moderngl.TRIANGLE_STRIP)
     def render_ui(self):
-        if not self.ui_elements:
+        if not self.ui_elements and not self.text_elements:
             return
+
         self.ctx.disable(moderngl.DEPTH_TEST)
-        # self.ctx.disable(moderngl.CULL_FACE)  # if you enabled it elsewhere
         self.ctx.enable(moderngl.BLEND)
-        ui_elements = []
-        for v in self.ui_elements:
-            ui_elements.append(v.vertices())
-        all_vertices = np.concatenate(ui_elements).astype('f4', copy=False)
-        self.ui_vbo.orphan(all_vertices.nbytes)
-        self.ui_vbo.write(all_vertices)
+
+        render_items = []
+        render_items.extend(self.ui_elements)
+        render_items.extend(self.text_elements)
+
+        render_items.sort(key=lambda e: e.layer)
+
+        for item in render_items:
+            if isinstance(item, Box):
+                self._render_ui_element(item)
+            else:
+                self._render_text_element(item)
+
+        self.ctx.enable(moderngl.DEPTH_TEST)
+
+    def flush_ui(self):
+        self.ui_elements = []
+        self.text_elements = []
+
+    def _render_ui_element(self, element):
+        vertices = element.vertices().astype('f4')
+
+        self.ui_vbo.orphan(vertices.nbytes)
+        self.ui_vbo.write(vertices)
 
         self.ui_prog["ortho"].write(self.ortho_projection.astype("f4").tobytes())
 
-        vertex_count = all_vertices.size // 6
+        vertex_count = vertices.size // 6
         self.ui_vao.render(mode=moderngl.TRIANGLES, vertices=vertex_count)
 
-        # self.ui_elements.clear()
-        self.ctx.enable(moderngl.DEPTH_TEST)
-
-    def render_text(self):
+    def _render_text_element(self, text):
         img = Image.new("RGBA", self.window_size, (0, 0, 0, 0))
         draw = ImageDraw.Draw(img)
 
-        for text in self.text_elements:
-            font_size = int(64 * text.scale)
-            font = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", font_size)
-            draw.text(
-                text.center,
-                text.text,
-                font=font,
-                fill=text.color + (int(255 * text.opacity),)
-            )
+        font_size = int(64 * text.scale)
+        font = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", font_size)
+
+        draw.text(
+            text.center,
+            text.text,
+            font=font,
+            fill=text.color + (int(255 * text.opacity),),
+            anchor="mm"
+        )
 
         text_data = np.array(img).astype('u1')
 
-        # ✅ Update existing texture instead of recreating it
         self.texture.write(text_data.tobytes())
         self.texture.use(location=0)
         self.text_vao.render(moderngl.TRIANGLE_STRIP)
 
-    # def render_box(self):
-    #
-    #     # === 1. Create text image using Pillow ===
-    #     img = Image.new("RGBA", self.window_size, (0, 0, 0, 0))
-    #     draw = ImageDraw.Draw(img)
-    #
-    #     for box in tuple(self.UI_elements):
-    #         w,h = box.size[0]/2, box.size[1]/2
-    #         x, y = box.center[0], box.center[1]
-    #         rectangle_coords = (box.center[0], box.center[1])
-    #         draw.rectangle(rectangle_coords, fill="blue", outline="red", width=3)
-    #
-    #     text_data = np.array(img).astype('u1')
-    #
-    #     # === 2. Upload to OpenGL as a texture ===
-    #     self.texture = self.ctx.texture(self.window_size, 4, text_data.tobytes())
-    #     self.texture.use()
-
-    def addUI(self, element):
-        self.UI_elements.add(element)
-
-    def removeUI(self, element):
-        self.UI_elements.add(element)
-
-    def renderUI(self):
-        for element in self.UI_elements:
-            if type(element) == Text():
-                self.render_text()
-
     def add_text_rect(self, text):
         self.text_elements.append(text)
 
-    def remove_text_rect(self, text):
-        self.text_elements = [t for t in self.text_elements if t is not text]
-
     def add_ui_rect(self, box):
         self.ui_elements.append(box)
-    def on_render(self, time: float, frametime: float):
+
+    def rendering_setup(self):
         if self.camera_obj.World.RunningFlag[0]:
             self.wnd.close()
+
         # collect all scene objects (root + children)
         scene_objs = self.root_object.get_all_children()
 
@@ -497,92 +510,28 @@ class BereshitRenderer(moderngl_window.WindowConfig):
             PyrrVector3(target.tolist()),
             PyrrVector3(up.tolist())  # or [0,1,0] if not rotated
         )
+        return cam_pos, target, up, shading
 
+    def on_render(self, time: float, frametime: float):
+        cam_pos, target, up, shading = self.rendering_setup()
         for item in self.meshes:
-            obj = item['obj']
-            pos = obj.position.to_np()
-            size = obj.size.to_np()
-            rot = obj.quaternion
-
-            # Use object's rotation
-            pyrr_obj_q = PyrrQuat([rot.x, rot.y, rot.z, rot.w])
-            # obj_rot_matrix = Matrix44.from_quaternion(pyrr_obj_q)
-
-            model = (
-                    Matrix44.from_scale(size * 0.5)
-                    @ Matrix44.from_quaternion(PyrrQuat([rot.x, rot.y, rot.z, rot.w]))
-                    @ Matrix44.from_translation(pos)
-            )
-            if shading == "wire":
-                item['vao'].render(mode=moderngl.LINES, vertices=item['len'])
-                self.wire_prog['model'].write(model.astype('f4').tobytes())
-                self.wire_prog['view'].write(self.view.astype('f4').tobytes())
-                self.wire_prog['projection'].write(self.projection.astype('f4').tobytes())
-                # self.prog['lightPos'].value = cam_pos  # your light source coordinates
-
-                color = obj.material.color if hasattr(obj.material, 'color') else (1.0, 1.0, 1.0)
-                self.wire_prog['color'].value = color
-            elif shading == "solid":
-                # Enable depth test
-                self.ctx.enable(moderngl.DEPTH_TEST)
-
-                # Set uniforms before drawing
-                self.solid_prog['model'].write(model.astype('f4').tobytes())
-                self.solid_prog['view'].write(self.view.astype('f4').tobytes())
-                self.solid_prog['projection'].write(self.projection.astype('f4').tobytes())
-
-                self.solid_prog['light_pos'].value = cam_pos
-
-                self.solid_prog['light_color'].value = (1.0, 1.0, 1.0)
-                color = obj.material.color if hasattr(obj.material, 'color') else (1.0, 1.0, 1.0)
-                self.solid_prog['object_color'].value = color
-                self.solid_prog['view_pos'].value = tuple(cam_pos)
-
-                # Render as filled triangles
-                item['vao'].render(mode=moderngl.TRIANGLES, vertices=item['len'])
-            elif shading == "material preview":
-                self.ctx.enable(moderngl.DEPTH_TEST)
-
-                # Set matrices
-                self.material_prog['model'].write(model.astype('f4').tobytes())
-                self.material_prog['view'].write(self.view.astype('f4').tobytes())
-                self.material_prog['projection'].write(self.projection.astype('f4').tobytes())
-
-                # Lighting uniforms
-                # self.material_prog['light_pos'].value = tuple(cam_pos)
-                # self.material_prog['light_color'].value = (1.0, 1.0, 1.0)
-                # self.material_prog['view_pos'].value = tuple(cam_pos)
-
-                # Bind texture
-                texture = obj.Mesh.texture()
-
-                if texture:
-                    if not hasattr(obj.Mesh, "gpu_texture"):
-                        obj.Mesh.gpu_texture = self.image_to_texture(texture)
-
-                    obj.Mesh.gpu_texture.use(location=0)
-                else:
-                    self.default_texture.use(location=0)
-                # Draw mesh
-                item['vao'].render(mode=moderngl.TRIANGLES, vertices=item['len'])
-        # Desired rectangle size
-        rect_width = 400
-        rect_height = 300
-
-        # Center position
-        x = (self.window_size[0] - rect_width) / 2
-        y = (self.window_size[1] - rect_height) / 2
-        # self.add_ui_rect(x, y, rect_width, rect_height, color=(1.0, 0.0, 0.0))
+            self.render_mesh(item, shading, cam_pos)
 
         # --- Render UI on top ---
         ### memory leak on the ui
-        self.render_text()
-        # self.render_ui()
+        render_items= []
+        render_items.extend(self.ui_elements)
+        render_items.extend(self.text_elements)
+        render_items.sort(key=lambda e: e.layer)
+        self.render_ui()
+        # self.render_text()
+        # self.render_elements()
         # self.text_vbo.render(moderngl.TRIANGLE_STRIP)
         ### memory leak on the ui
 
-
-
+    def on_key_event(self, key, action, modifiers):
+        if action != 0:
+            self.text_input = key
 def run_renderer(root_object, Initialize, Exit):
     BereshitRenderer.Initialize = Initialize
     BereshitRenderer.Exit = Exit
@@ -592,7 +541,7 @@ def run_renderer(root_object, Initialize, Exit):
 
 class Text:
     def __init__(self, text="", center=(0.0, 0.0), size=(512, 128), scale=1.0, color=(255, 255, 255), opacity=1,
-                 container=None):
+                 container=None, layer=0):
         self.container = container
         self.text = text
         self.center = center
@@ -600,18 +549,28 @@ class Text:
         self.scale = scale
         self.color = color
         self.opacity = opacity
+        self.layer = layer
 
 
 class Box:
-    def __init__(self, center=(960, 540), size=(100, 100), scale=1.0, color=(255, 255, 255), opacity=1, container=None,
-                 children=None):
+    def __init__(self, center=(960, 540), size=(100, 100), scale=1.0, color=(255, 255, 255), opacity=1, layer=0, container=None,
+                 children=None , clickable=False):
         self.container = container
         self.children = children
-        self.center = center
+        self.center = (center[0], 1080 - center[1])
         self.size = size
         self.scale = scale
-        self.color = color
+        self.color = (color[0] / 255, color[1] / 255, color[2] / 255)
         self.opacity = opacity
+        self.layer = layer
+
+    def click(self, position):
+        return (
+                self.center[0] - self.size[0] / 2 <= position[0] <= self.center[0] + self.size[0] / 2 and
+                -self.center[1] + 1080 - self.size[1] / 2 <= position[1] <= -self.center[1] + 1080 + self.size[1] / 2
+        )
+
+
 
     def vertices(self):
         r, g, b = self.color
